eed53ac6600e200fcbba4143d34237c5cba1ed03
Jaime Vicencio <jai.vicencio@duocuc.cl>
2023-12-10 11:36:17 -0300

    requirements update

--- ms_galenos_web_genera_correos/settings.py
+++ ms_galenos_web_genera_correos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-5f36c3fhlj@3#*8u%!^itc8g+ju@-8hk+9a_v*3l=4dptfy%+m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["52.3.91.207", "127.0.0.1"]
 
 
 # Application definition
@@ -74,11 +74,25 @@ WSGI_APPLICATION = "ms_galenos_web_genera_correos.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "api_rest_django",
+        "USER": "mysql",
+        "PASSWORD": "password",
+        "HOST": "mysql_db",
+        "PORT": "3306",
+        "OPTIONS": {
+            "sql_mode": "traditional",
+        },
+    },
 }
 
 
